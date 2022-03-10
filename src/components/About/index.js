7c5a728c08c407c6eff4ef5950adfca0a9680dc3
Shahabgeravesh <Shahabgeravesh@gmail.com>
2022-03-09 23:25:58 -0800

    Education,techskills,about foundation

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function About() {
+    return (
+        <div >
+            <h3 className="d-flex" >About</h3>
+            <img src="https://github.com/shahabgeravesh.png"></img>
+        </div>
+    );
+}
+export default About;
